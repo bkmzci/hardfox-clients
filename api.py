0606159472a89bec29b1e175dba8663aeaa09217
casdqw <109471966+bkmzci@users.noreply.github.com>
2022-10-28 17:00:26 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+
+
+
+token = input("Token:")
+API = "http://185.242.163.237:5000/hardfox/api/v1"
+
+def create_machine():
+    name = input("machine name:")
+    machinies = {"name": name,"token":token}
+    req = requests.post(API+"/machines",json=machinies)
+    print(req.content)
+    req.close()
+
+    json_object = json.dumps(machinies, indent=4)
+    with open("config.json", "w") as outfile:
+       outfile.write(json_object)
+
+    return name
+
+
+
+def get_machine(name):
+    req = requests.get(API+"/machines?name="+name)
+    response_json = json.loads(req.text)
+    return response_json
+
+
+
+def update_machine_status(machine_name,status):
+    data = {"name":machine_name,"status":status,"token":token}
+    req = requests.post(API+"/machines/status", json=data)
+    print(req.content)
+    req.close()
+
+
+def add_subdomain_to_db(domain,subdomain):
+    data = {"domain": domain, "subdomain": subdomain,"token":token}
+    req = requests.post(API+"/subdomains",json=data)
+    print(req.content)
+    req.close()
+
+
